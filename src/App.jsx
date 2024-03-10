eb30904e0da97f7f6117b6aebf034523772bbb68
Aman Roy <yadavaman7632@gmail.com>
2024-03-10 19:46:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useState,useEffect } from 'react'
+import Card from './components/Card';
+function App() {
+
+  const [searchText, setSearchText] = useState("");
+  const [product,setProduct]=useState([]);
+  const submitHandle=(e)=>{
+    e.preventDefault();
+    const filterData=product.filter((item)=>(item.name.toLowerCase().includes(searchText.toLowerCase())))
+    // console.log(filterData);
+    setProduct(filterData)
+  }
+  useEffect(() => {
+    fetch(`https://api.punkapi.com/v2/beers`)
+      .then((res) => (res = res.json()))
+      .then((data) => {
+        // console.log(data);
+        setProduct(data);
+      })
+      .catch((error) => alert("Internet Error"));
+  }, [searchText]);
+  return (
+    <>
+    <nav className='w-full h-16 bg-black text-white flex justify-around'>
+      <div className="log w-1/2 flex items-center">
+        <h2>Logo</h2>
+        </div>
+      <ul className='flex w-1/5 justify-between items-center'>
+        <li>Home</li>
+        <li>About</li>
+        <li><a href="https://github.com/Amanroy7632" target='_blanck'>Github</a></li>
+      </ul>
+      </nav>
+    <div className='w-full'>
+      <div className='bg-gray-400 flex justify-center border p-4'>
+        <form onSubmit={submitHandle} className='flex'>
+        <input type="text" className=' text-2xl w-full border-none outline-green-300' placeholder='Search items...' value={searchText} onChange={(e)=>(setSearchText(e.target.value))}  />
+        <button className=' full bg-transparent text-white text-2xl border rounded-r-xl pl-1'>🔍</button>
+        </form>
+      </div>
+       <h1 className=' text-center p-3 text-2xl bg-black text-white'>Products</h1>
+    <div className=' flex justify-around flex-wrap'>
+      
+    {
+      product.map((product)=>{
+        return <Card key={product.id} product={product}/>
+      })
+    } 
+    </div>
+    {product.length===0?<div><h1 className=' text-2xl text-center bg-slate-300'>Result not Found ..</h1> </div>:""}
+
+
+    
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+function Card({product}) {
+  return (
+    <div
+    
+      key={product.id}
+      className="w-1/4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+    >
+      
+      <a href='/'>
+        <img
+          className="p-8 rounded-t-lg w-4/5 aspect-square m-auto max-sm:p-0 max-sm:rounded-lg max-sm:w-2/3"
+          src={product.image_url}
+          alt="product_image1"
+          style={{
+            // width: "80%",
+            backgroundBlendMode: "color-burn",
+            objectFit:"cover",
+          }}
+        />
+      </a>
+      <div className="px-5 pb-5 max-sm:pb-3  max-sm:pt-0">
+        {/* <a href="/"> */}
+          <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white max-sm:pt-0">
+            {product.name}
+          </h5>
+          <p className=' text-white'>{product.tagline}</p>
+        {/* </a> */}
+        <div className="flex items-center mt-2.5 mb-5 max-sm:mt-1.2 max-sm:mb-1">
+          <span className=' text-white'>Abv</span>
+          <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
+            {product.abv}
+          </span>
+        </div>
+        <div className="flex items-center justify-between">
+          <span className="text-3xl font-bold text-gray-900 dark:text-white">
+            🪙 {product.attenuation_level}
+          </span>
+        
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card
